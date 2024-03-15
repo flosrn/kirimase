fa80535ccff6927827a380285b4878016b16bc55
Flosrn <flosrn.dev@gmail.com>
2024-03-15 10:29:30 +0100

    first commit

---
+++ src/lib/auth/utils.ts
@@ -0,0 +1,56 @@
+import { db } from "@/lib/db/index";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { DefaultSession, getServerSession, NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import { redirect } from "next/navigation";
+import { env } from "@/lib/env.mjs"
+import GitHub from "next-auth/providers/github";
+
+
+declare module "next-auth" {
+  interface Session {
+    user: DefaultSession["user"] & {
+      id: string;
+    };
+  }
+}
+
+export type AuthSession = {
+  session: {
+    user: {
+      id: string;
+      name?: string;
+      email?: string;
+    };
+  } | null;
+};
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  callbacks: {
+    session: ({ session, user }) => {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  providers: [
+    GitHub({
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_SECRET,
+      // This is required because AuthJS beta is not stable.
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+};
+
+
+export const getUserAuth = async () => {
+  const session = await getServerSession(authOptions);
+  return { session } as AuthSession;
+};
+
+export const checkAuth = async () => {
+  const { session } = await getUserAuth();
+  if (!session) redirect("/api/auth/signin");
+};
+
